92e3fbd50fe8e120f863505e58bfc0de992872f9
Francisco Dominguez Mateos <francisco.dominguez@urjc.es>
2019-07-25 23:34:04 +0200

    little changes

--- pyHotDraw/src/pyHotDraw/Figures/pyHCompositeTransform2DFigure.py
+++ pyHotDraw/src/pyHotDraw/Figures/pyHCompositeTransform2DFigure.py
@@ -2,8 +2,8 @@
 Created on 16 Jul 2019
 
 @author: Francisco Dominguez
-+ Added transformation2D
-- TODO: getDisplayBox and move doesn't seem to work with 2Dtransformations
+This is like a drawing but you can put in any place of the main drawing and scale it too
+- move and getDisplayBox are not recursive but draw is
 '''
 from pyHAbstractFigure import pyHAbstractFigure
 from pyHotDraw.Geom.pyHTransform2D import pyHTransform2D
@@ -16,7 +16,6 @@ class pyHCompositeTransform2DFigure(pyHCompositeFigure):
     def __init__(self,t=pyHTransform2D()):
         pyHCompositeFigure.__init__(self)
         self.t=t
-        
     def draw(self,g):
         # save trasnformation
         oldt=g.getTransformation()
@@ -27,11 +26,9 @@ class pyHCompositeTransform2DFigure(pyHCompositeFigure):
             f.draw(g)
         # restore transformation
         g.setTransformation(oldt)
-    # TODO
     def move(self,dx,dy):
         #pyHCompositeFigure.move(self,dx,dy)
-        self.t=self.t+pyHTransform2D(1,1,dx,dy)
-    # TODO   only one transformation not recursion as in draw 
+        self.t=self.t+pyHTransform2D(1,1,dx,dy) 
     def getDisplayBox(self):
         r=pyHCompositeFigure.getDisplayBox(self)
         rx,ry=self.t.transform(r.getX(),r.getX())
